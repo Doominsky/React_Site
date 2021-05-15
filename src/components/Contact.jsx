5972441548a20db8110addb3e4b79d47229e343e
Andrei <andrei0799@gmail.com>
2021-05-15 15:56:43 +0300

    First contact page version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,23 +3,44 @@ import React from "react";
 function Contact() {
   return (
     <div className="contact">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Contact</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+      <div className="container">
+        <div>
+          
+          <div align="center">
+            <h1 class="font-weight-light" align="center">Contact</h1>
+              <form name="form1" action="MAILTO:andrei_iatin@yahoo.com" method="post" enctype="text/plain">
+                <font/>Name: <br/>
+                  <input type="text" name="nume" /><br />
+                  Surname: <br/>
+                  <input type="text" name="adresa" /><br /><br/>
+                  What programming language do you see me fit for your job position?<br/>
+                  <input type="checkbox" name="job[]"  value="html"/> HTML/CSS/JS<br/>
+
+                  <input type="checkbox" name="job[]"  value="java"/> Java<br/>
+
+                  <input type="checkbox" name="job[]"  value="cplusplus"/> C++<br/>
+
+                  <br />
+                  What company are you representing?<br/>
+                  <select name="pg">
+                  <option value="">Select one</option>
+                  <option value="Haufe">Haufe Group</option>
+                  <option value="Haufe">Atoss Software</option>
+                  <option value="Haufe">Fastbit</option>
+                  <option value="Haufe">Other</option>
+                  </select><br/>
+
+                  <br />
+                  If other, type it below.<br/>
+                  <textarea name="other"></textarea><br/><br/>
+                  Send a message<br/>
+                  <textarea name="message"></textarea><br /><br/>
+
+                  <input type="submit"  value="Send" />
+
+                  <input type="reset"  value="Reset"/>
+
+            </form>
           </div>
         </div>
       </div>
